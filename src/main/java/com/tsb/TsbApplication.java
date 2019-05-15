e954d9b7ebb0e61bcb26b442442ca5079f3a6a52
m-arulraj <arulraj.murugesan@gmail.com>
2019-05-15 10:29:14 +0530

    test

--- src/main/java/com/tsb/TsbApplication.java
+++ src/main/java/com/tsb/TsbApplication.java
@@ -14,6 +14,7 @@ public class TsbApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("TSB is running...");
 		SpringApplication.run(TsbApplication.class, args);
 	}
 
