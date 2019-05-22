1c6db3f1645a7544a630842b7c17cd1df4b4dd24
m-arulraj <arulraj.murugesan@gmail.com>
2019-05-22 11:54:36 +0530

    changed the type

--- src/main/java/com/tsb/domain/Category.java
+++ src/main/java/com/tsb/domain/Category.java
@@ -22,7 +22,7 @@ public class Category extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "categoryid")
 	private Long categoryId;
 
