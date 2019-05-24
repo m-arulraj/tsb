4a18b70dbf7167ee3e5426ab80dc3e75431af094
m-arulraj <arulraj.murugesan@gmail.com>
2019-05-24 19:05:35 +0530

    db logic

--- src/main/java/com/tsb/TsbApplication.java
+++ src/main/java/com/tsb/TsbApplication.java
@@ -1,9 +1,15 @@
 package com.tsb;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import liquibase.integration.spring.SpringLiquibase;
 
 @SpringBootApplication
 public class TsbApplication extends SpringBootServletInitializer {
@@ -15,6 +21,15 @@ public class TsbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TsbApplication.class, args);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(@Autowired DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(dataSource);
+		liquibase.setChangeLog("classpath:config/liquibase/teb-schema.xml");
+		liquibase.setShouldRun(true);
+		return liquibase;
 	}
 
 }
